cf4ca8954d530bbd24aefe7e7a27ff766222e655
kamada-oss <kamada.engineer@gmail.com>
2020-10-03 16:23:16 +0900

    deviceを導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_27_164133) do
+ActiveRecord::Schema.define(version: 2020_10_03_071911) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "name"
@@ -44,6 +44,12 @@ ActiveRecord::Schema.define(version: 2020_09_27_164133) do
     t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comment_categories", "categories"
