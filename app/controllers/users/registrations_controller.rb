6c1ba644248f25c72488cc098ea3ec7cbc6f1548
kamada-oss <kamada.engineer@gmail.com>
2020-10-04 23:28:29 +0900

    新規登録機能を実装

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,24 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to categories_index_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(
+        :nickname,
+        :email,
+        :password,
+        :password_confirmation
+      )
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :goods, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :nickname, presence: true, length: {maximum: 20},
+            uniqueness: true
+  validates :email, presence:true, uniqueness: true, 
+            format: { with: VALID_EMAIL_REGEX, message: 'のフォーマットが不適切です'}
 end
