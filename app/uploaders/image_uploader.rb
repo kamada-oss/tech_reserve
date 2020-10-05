416732c929f71d8f91f73300552327c929991d88
kamada-oss <kamada.engineer@gmail.com>
2020-10-05 23:10:38 +0900

    本番環境のみS3を使用するよう修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,12 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  if Rails.env.development? || Rails.env.test? 
+    storage :file
+  else
+    storage :fog
+  end
+
   include CarrierWave::MiniMagick
   process resize_to_fit: [800, 800]
   # Override the directory where uploaded files will be stored.
